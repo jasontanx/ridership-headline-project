b577abcdcf9176dd7cb91e4132b65f91390caf79
Jason Tan <116934441+jasontanx@users.noreply.github.com>
2023-10-06 14:11:48 +0800

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,6 @@
+# MinIO configuration
+minio_endpoint = ${MINIO_ENDPOINT}
+minio_access_key = ${MINIO_ACCESS_KEY}
+minio_secret_key = ${MINIO_SECRET_KEY}
+minio_bucket_name = "ridership-headline-data"
+minio_file_name = "ridership_headline.csv"  # The name you want to give to the file in MinIO
